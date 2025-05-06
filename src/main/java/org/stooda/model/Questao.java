dde9e008c424b3943969b7128ce66b434408eb4a
danielgut763 <daniel.gutschwager@gmail.com>
2025-05-06 08:51:36 -0300

    question class name

--- src/main/java/org/stooda/model/Questao.java
+++
@@ -1,8 +0,0 @@
-package org.stooda.model;
-
-import java.util.List;
-
-public class Questao {
-    private String enunciado;
-    private List<Alternative> alternativas;
-}
---
+++ src/main/java/org/stooda/model/Question.java
@@ -0,0 +1,8 @@
+package org.stooda.model;
+
+import java.util.List;
+
+public class Question {
+    private String questionText;
+    private List<Alternative> alternatives;
+}

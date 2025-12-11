f47c0d46b778ac6916a0d6e06d9c04de3918094d
danielgut763 <daniel.gutschwager@gmail.com>
2025-12-11 09:17:59 -0300

    fresh start

--- src/main/java/org/stooda/model/Alternative.java
+++
@@ -1,7 +0,0 @@
-package org.stooda.model;
-
-public class Alternative {
-    private String description;
-    private String imagePath;
-    private Boolean correct;
-}
--- src/main/java/org/stooda/model/Question.java
+++
@@ -1,8 +0,0 @@
-package org.stooda.model;
-
-import java.util.List;
-
-public class Question {
-    private String questionText;
-    private List<Alternative> alternatives;
-}
